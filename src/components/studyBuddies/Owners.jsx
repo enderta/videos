f06931ee78bd2dc5eee48c76ffed031dbefe08ca
enderta <endertanver@gmail.com>
2023-02-22 13:33:30 +0000

    email

--- src/components/studyBuddies/Owners.jsx
+++ src/components/studyBuddies/Owners.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import Owner from "./Owner";
-import {Modal, Table} from "react-bootstrap";
-import CreateOwner from "./CreateOwner";
-import { UserProvider } from './UserContext';
 import {Link} from "react-router-dom";
 
 const Owners = (props) => {
 
     const [owners, setOwners] = React.useState([]);
-    const [show, setShow] = React.useState(false);
 
     React.useEffect(() => {
         fetch('http://localhost:3001/availabilities',
@@ -31,26 +27,19 @@ const Owners = (props) => {
         localStorage.removeItem('token');
         window.location.href = 'http://localhost:3000';
     }
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
     return (
         <div>
-
             <h1 style={{color: "yellow", textAlign: "center"}}>List of Trainees</h1>
             <div>
                   <span>
-
                 <button onClick={handleLogout} className="btn btn-outline-info"
                         style={{float: "right", margin: "10px"}}>Logout</button>
-                      <button className="btn btn-outline-info" >
-                         <Link to='/availability' style={{color: "white"}}>Create Owner</Link>
+                      <button className="btn btn-outline-info">
+                         <Link to='/availability' style={{float: "left", margin: "10px",textDecoration:'none'}}>Add Availability</Link>
                       </button>
-
             </span>
             </div>
-
-
             <div className="container">
                 <table
                     className="table table-responsive table-dark table-borderless table-striped table-bordered table-hover">
@@ -66,9 +55,6 @@ const Owners = (props) => {
                     {owners.map(owner =>
                         <Owner key={owner.id} owner={owner} owners={owners} setOwners={setOwners}/>)
                     }
-
-
-
                     </tbody>
                 </table>
             </div>
