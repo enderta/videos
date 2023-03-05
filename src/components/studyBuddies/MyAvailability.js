7e727f968d93bf77f26612d8bf00c6a02cc3c22d
enderta <endertanver@gmail.com>
2023-03-05 18:45:13 +0000

    new

--- src/components/studyBuddies/MyAvailability.js
+++ src/components/studyBuddies/MyAvailability.js
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import NavBar from "./NavBar";
 import Button from "react-bootstrap/Button";
+import {Form} from "react-bootstrap";
+import AvailibityTable from "./AvailibityTable";
+import MyAvailabilityCard from "./MyAvailabilityCard";
 
 const MyAvailability = () => {
-    const [myDate, setMyDates] = useState([]);
-    const [edit, setEdit] = useState(false);
-    const [date, setDate] = useState("");
-    const [topic, setTopic] = useState("");
 
-
-
+    const[trainees_id, setTrainees_id] = useState(localStorage.getItem("id"));
+    const[myDates, setMyDates] = useState([]);
     useEffect(() => {
         fetch(`https://study-buddies.onrender.com/availability/${localStorage.getItem("id")}`, {
             method: "GET",
@@ -23,88 +22,41 @@ const MyAvailability = () => {
                 setMyDates(data);
             })
             .catch((err) => console.log(err));
-    },[localStorage.getItem("av_id")]);
-    console.log(myDate);
-    const handleDelete = (id) => {
-        fetch(`https://study-buddies.onrender.com/availability/${id}`, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: localStorage.getItem("token"),
-            }
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setMyDates(myDate.filter((date) => date.id !== id));
-            })
-            .catch((err) => console.log(err));
-    }
-    const handleEdit = (id) => {
-        fetch(`https://study-buddies.onrender.com/availability/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: localStorage.getItem("token"),
-            },
-            body: JSON.stringify({
-                availability_date: date,
-                topic: topic,
-            })
-        })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log(data);
-                setMyDates(myDate.map((date) => date.id === id ? { ...date, availability_date: date, topic: topic } : date));
-                setEdit(!edit);
-            })
-            .catch((err) => console.log(err));
-    }
-
+    }, []);
 
     return (
         <div>
             <div>
-                <NavBar />
+                <NavBar/>
             </div>
             <h1>My Availability</h1>
             <div>
                 <div>
                     <div className="container">
-                        <div className="card-body">
-                            <div className="row">
-                                {myDate.map((date) => (
-                                    <div className="col-md-4">
-                                        <div className="card mb-4 shadow-sm">
-                                            <div className="card-body">
-                                                <p className="card-text">
-                                                    <strong>Date: </strong>
-                                                    {new Date(date.availability_date).toLocaleDateString()}
-                                                </p>
-                                                <p className="card-text">
-                                                    <strong>Topic: </strong>
-                                                    {date.topic}
-                                                </p>
-                                                <Button className={"btn-outline-danger"} onClick={() => handleDelete(date.id)}>Delete</Button>
-                                               {/* <Button className={"btn-outline-primary"} onClick={() => setEdit(!edit)}>Edit</Button>
-                                                {edit && (
-                                                    <div>
-                                                        <input type="date" onChange={(e) => setDate(e.target.value)} />
-                                                        <input type="text" onChange={(e) => setTopic(e.target.value)} />
-                                                        <Button className={"btn-outline-primary"} onClick={() => handleEdit(date.id)}>Submit</Button>
-                                                    </div>
-                                                )}*/}
-                                            </div>
+                        <div className="row">
+                            {myDates.map(date => {
+                                return (
+                                    <div className="col-md-6">
+                                        <div className="card mb-4 shadow-sm" style={{
+                                            background: "darkgoldenrod",
+                                            border: "black solid 1px",
+                                            margin: '2px',
+                                            padding: '2px'
+                                        }}>
+                                            <MyAvailabilityCard key={date.id} date={date} dates={myDates}
+                                                                setDates={setMyDates}
+
+                                                            />
                                         </div>
                                     </div>
-                                ))}
-                            </div>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     );
-};
 
+};
 export default MyAvailability;
---
+++ src/components/studyBuddies/MyAvailabilityCard.jsx
@@ -0,0 +1,115 @@
+import React, {useState} from 'react';
+import NavBar from "./NavBar";
+import {Form, Modal} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+const MyAvailabilityCard = (props) => {
+    const [edit, setEdit] = useState(false);
+    const [trainees_id, setTrainees_id] = useState(localStorage.getItem("id"));
+    const [date, setDate] = React.useState(new Date());
+    const [topic, setTopic] = React.useState('');
+
+    const handleEdit = (e) => {
+        e.preventDefault();
+        setEdit(true);
+    }
+    const handleDelete = (e) => {
+        e.preventDefault();
+        fetch(`https://study-buddies.onrender.com/availability/${props.date.id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem('token')
+            }
+        }).then(res => res.json())
+            .then(data => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        alert('You have deleted this available date!');
+                        window.location.reload();
+                    }
+                }
+            )
+            .catch(err => console.log(err))
+    }
+    const handleUpdate = (e) => {
+        e.preventDefault();
+        const formattedDate = `${date.getFullYear()}-${('0' + (date.getMonth() + 1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`;
+        const available = {
+            availability_date: formattedDate,
+            topic: topic,
+            trainees_id: trainees_id
+        }
+        fetch(`https://study-buddies.onrender.com/availability/${props.date.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: localStorage.getItem('token')
+            },
+            body: JSON.stringify(available)
+        }).then(res => res.json())
+            .then(data => {
+                    if (data.error) {
+                        alert(data.error);
+                    } else {
+                        alert('You have updated this available date!');
+                        window.location.reload();
+                    }
+                }
+            )
+            .catch(err => console.log(err))
+    }
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        if (name === 'date') {
+            setDate(new Date(value));
+        } else if (name === 'topic') {
+            setTopic(value);
+        }
+    }
+    return (
+        <div>
+            <div className={"card"}>
+                <div className={"card-body"}>
+                    <h5 className="card-title">  Date:  {new Date(props.date.availability_date).toLocaleDateString()}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">Topic: {props.date.topic}</h6>
+                    <Button style={{margin:"10px"}} variant="outline-danger" onClick={handleDelete}>Delete</Button>
+                    <Button variant="outline-primary" onClick={()=>setEdit(!setEdit())}>Edit</Button>
+                    <Modal show={edit} onHide={()=>setEdit(!edit)}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Edit Availability</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form style={{ width: '50%', margin: 'auto' }}>
+                                <Form.Group controlId="formBasicDate">
+                                    <Form.Label style={{color:"goldenrod"}}>Date</Form.Label>
+                                    <Form.Control type="date"
+                                                  min={new Date().toISOString().split('T')[0]}
+                                                  max={new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString().split('T')[0]}
+                                                  name="date" onChange={handleChange} />
+                                </Form.Group>
+                                <Form.Group controlId="formBasicTopic">
+                                    <Form.Label style={{color:"goldenrod"}}>Topic</Form.Label>
+                                    <Form.Control type="text" placeholder="Topic" name="topic" onChange={handleChange} />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button style={{margin:"10px"}} variant="secondary" onClick={()=>setEdit(!edit)}>
+                                Close
+                            </Button>
+                            <br/>
+                            <Button variant="primary" onClick={handleUpdate}>
+                                Save Changes
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default MyAvailabilityCard;
