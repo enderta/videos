ff1fd2b61cd1cf61175293c2c44478e518a6aca6
enderta <endertanver@gmail.com>
2023-08-24 18:20:42 +0100

    "Refactor App.js, update .idea updates to our component configuration for enhanced user interface.
    Removed the XML version encoding from .idea/misc.xml. This change is a part of the maintenance process to clean up unnecessary code and improve the readability."

--- src/components/moives-series/HomeNavi.jsx
+++ src/components/moives-series/HomeNavi.jsx
@@ -1,29 +1,19 @@
 import React from 'react';
+import image from './pexels-markus-spiske-1089438.jpg';
 
 const HomeNavi = () => {
     return (
-        <div>
-            <div>
-                <video autoPlay loop muted>
-                    <source
-                        src={require('./pexels-gamol-10532470.mp4')}
-                        type="video/mp4"
-                    />
+        <div className="home_navi" >
 
-                </video>
-                <div className="container">
-                    <button type="button" className='home_link1'  >
-                       <a className='home_link1_a'  href="/series" > <h1 className="link_text1" >Series</h1></a>
-                    </button>
-                    <button type="button" className='home_link2'  >
-                      <a className='home_link2_a' href="/movies" >  <h1 className="link_text2" >Movies</h1></a>
-                    </button>
-                </div>
-
+            <div className="container">
+                <button type="button" className='home_link1'>
+                    <a className='home_link1_a' href="/series"> <h1 className="link_text1">Series</h1></a>
+                </button>
+                <button type="button" className='home_link2'>
+                    <a className='home_link2_a' href="/movies"> <h1 className="link_text2">Movies</h1></a>
+                </button>
             </div>
         </div>
-
-
     );
 };
 
